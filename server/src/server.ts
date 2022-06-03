de5893df996a6e1556c9d2bbb25fb54e54613e77
AzenaideFernandes <azenaidefkt@gmail.com>
2022-06-03 12:35:47 -0400

    Commit 03
    Refazendo commit após aula 03.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,61 +1,14 @@
 import express from 'express'
-import { prisma } from './prisma'; 
+import cors from 'cors'
+import { routes } from './routes';
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
-/* app.get('/users', (req, res) =>  {
-  return res.send('Helo Word');
-}) */
-
-/* app.post('/feedbacks', (req, res) =>  {
-  //console.log(req.body);
-  const{type, comment, screenshot} = req.body;
-  const feedback =  prisma.feedback.create({
-    data: {
-      type: req.body.type,
-      comment: req.body.comment,
-      screenshot: req.body.screenshot,
-    }
-  }) 
-  return res.status(201).json({ data: feedback });
-}); */
-
-app.post('/feedbacks', async (req, res) =>  {
-  //console.log(req.body);
-  const { type, comment, screenshot } = req.body;
-  const feedback = await prisma.feedback.create({
-    data: {
-      type,
-      comment,
-      screenshot,
-    }
-  }) 
-
-  return res.status(201).json({data: feedback });
-});
-
-  /* app.post('/feedbacks', async(req, res) =>  {
-    const{type, comment, screenshot} = req.body;
-    const feedback = await prisma.feedback.create({
-      data: {
-        type,
-        comment,
-        screenshot,
-      }
-    })
-  return res.status(201).json({ data: feedback});
-}); */
+app.use(routes);
+// GET, POST, PUT, PATCH, DELETE
 
 app.listen(3333, () => {
-  console.log('HTTP sever running!');
-
+  console.log("HTTP server running!");
 });
-
-// Prisma
-//SQLite
-// GET = Buscar Informações
-// POST = Cadastrar Informações
-// PUT =  Atualizar Informações de uma entidade
-// PATCH = Atualizar uma informação única de uma entidade
-// DELETE =  Deletar uma Informação
